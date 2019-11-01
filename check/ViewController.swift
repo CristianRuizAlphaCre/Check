1b66feb2528b91c747e0137dd003b6ad7c9c28d2
Cristian Ruiz <cristian.ruiz@alphacredit.co>
2019-10-31 21:30:57 -0500

    Initial Commit

---
+++ check/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  check
+//
+//  Created by Cristian David Ruiz Rodriguez on 10/31/19.
+//  Copyright © 2019 Alphacredit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ checkTests/checkTests.swift
@@ -0,0 +1,34 @@
+//
+//  checkTests.swift
+//  checkTests
+//
+//  Created by Cristian David Ruiz Rodriguez on 10/31/19.
+//  Copyright © 2019 Alphacredit. All rights reserved.
+//
+
+import XCTest
+@testable import check
+
+class checkTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
